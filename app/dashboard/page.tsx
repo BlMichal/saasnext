bb66a6d5641e99ef82a4cf9ab4303fd5dbd99e75
Michal Blažek <blaza0@email.cz>
2024-08-27 16:24:10 +0200

    v1.01 Kinde setup

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+
+
+
+
+const DashBoardPage = () => {
+  return (
+    <div><LoginLink>Sign in</LoginLink>
+<RegisterLink>Sign up</RegisterLink></div>
+  )
+}
+
+export default DashBoardPage
